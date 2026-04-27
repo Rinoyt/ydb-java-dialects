29fd907462da3ec60189ce8df4c8bd94fd76d067
Rinoyt <Sasha1Sim5@gmail.com>
2026-04-27 13:28:36 +0300

    fixed BatchTest

--- jimmer-dialect/src/test/java/ydb/jimmer/dialect/batch/BatchTest.java
+++ jimmer-dialect/src/test/java/ydb/jimmer/dialect/batch/BatchTest.java
@@ -1,58 +1,80 @@
 package ydb.jimmer.dialect.batch;
 
 import org.babyfish.jimmer.sql.ast.mutation.SaveMode;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ydb.jimmer.dialect.AbstractInsertTest;
-import ydb.jimmer.dialect.model.type.ydbInt32.YdbIntDraft;
+import ydb.jimmer.dialect.model.Entity;
+import ydb.jimmer.dialect.model.EntityDraft;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BatchTest  extends AbstractInsertTest {
     private static final String TABLE_NAME = "simple_table";
 
-    private void batchTest(SaveMode saveMode, String sql) {
+    private void batchTest(
+            SaveMode saveMode,
+            String sql,
+            int[][] batches,
+            int[][] expectedBatches
+    ) {
+        List<Entity> entities = new ArrayList<>();
+        for (int[] batch : batches) {
+            entities.add(EntityDraft.$.produce(t -> {
+                t.setId(batch[0]);
+                t.setValue(batch[1]);
+            }));
+        }
+
         executeAndExpect(
-                getYqlClientForBatch().getEntities().saveEntitiesCommand(
-                        Arrays.asList(
-                            YdbIntDraft.$.produce(t -> {
-                                t.setId(0);
-                                t.setValue(123);
-                            }),
-                            YdbIntDraft.$.produce(t -> {
-                                t.setId(1);
-                                t.setValue(456);
-                            })
-                        ))
+                getYqlClientForBatch()
+                        .getEntities()
+                        .saveEntitiesCommand(entities)
                         .setMode(saveMode),
                 cxt -> {
                     cxt.sql(sql);
-                    cxt.batchVariables(0, 0, 123);
-                    cxt.batchVariables(1, 1, 456);
+
+                    for (int i = 0; i < batches.length; i++) {
+                        cxt.batchVariables(i, expectedBatches[i][0], expectedBatches[i][1]);
+                    }
                 }
         );
     }
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         createTable(TABLE_NAME, "Int32");
+    }
+
+    @AfterEach
+    void teardown() {
+        dropTable(TABLE_NAME);
     }
 
     @Test
     public void insertTest() {
+        int[][] batches = new int[][]{{0, 123}, {1, 456}};
         batchTest(SaveMode.INSERT_ONLY,
-                "insert into " + TABLE_NAME + "(id, value) values(?, ?)");
+                "insert into " + TABLE_NAME + "(id, value) values(?, ?)",
+                batches, batches);
     }
 
     @Test
     public void updateTest() {
+        int[][] batches = new int[][]{{0, 123}, {1, 456}};
+        int[][] expectedBatches = new int[][]{{123, 0}, {456, 1}};
         batchTest(SaveMode.UPDATE_ONLY,
-                "update " + TABLE_NAME + " set value = ? where id = ? returning id");
+                "update " + TABLE_NAME + " set value = ? where id = ? returning id",
+                batches, expectedBatches);
     }
 
     @Test
     public void upsertTest() {
+        int[][] batches = new int[][]{{0, 123}, {1, 456}};
         batchTest(SaveMode.UPSERT,
-                "upsert into " + TABLE_NAME + "(id, value) values(?, ?)");
+                "upsert into " + TABLE_NAME + "(id, value) values(?, ?)",
+                batches, batches);
     }
 }
