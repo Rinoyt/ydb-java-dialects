0decbca90a02f3fd1b8f192fbb96262b40ba7695
Rinoyt <Sasha1Sim5@gmail.com>
2026-04-28 15:44:38 +0300

    In Insert tests the crreated SQL table is dropped at the end

--- jimmer-dialect/src/test/java/ydb/jimmer/dialect/dataTypeTest/InsertDataTypeTest.java
+++ jimmer-dialect/src/test/java/ydb/jimmer/dialect/dataTypeTest/InsertDataTypeTest.java
@@ -63,6 +63,8 @@ public class InsertDataTypeTest extends AbstractInsertTest {
                     cxt.variables(variables);
                 }
         );
+
+        dropTable(tableName);
     }
 
     @Test
