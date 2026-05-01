7b776a494ab92ca78f76ec0b113e121171fba7b9
Rinoyt <Sasha1Sim5@gmail.com>
2026-05-02 01:53:23 +0300

    Added more join types to the simple join test

--- jimmer-dialect/src/test/java/ydb/jimmer/dialect/JoinTest.java
+++ jimmer-dialect/src/test/java/ydb/jimmer/dialect/JoinTest.java
@@ -11,20 +11,39 @@ public class JoinTest extends AbstractSelectTest {
         initDatabase();
     }
 
-    @Test
-    public void leftJoinTest() {
+    private void joinTest(JoinType joinType, String join) {
         StudentTable table = StudentTable.$;
         executeAndExpect(
                 getYqlClient()
                         .createQuery(table)
-                        .orderBy(table.group(JoinType.LEFT).name().asc())
+                        .orderBy(table.group(joinType).name().asc())
                         .select(table),
                 cxt -> cxt.sql(
                         "select tb_1_.id, tb_1_.name, tb_1_.group " +
                                 "from student tb_1_ " +
-                                "left join group tb_2_ on tb_1_.group = tb_2_.id " +
+                                join + " join group tb_2_ on tb_1_.group = tb_2_.id " +
                                 "order by tb_2_.name asc"
                 )
         );
+    }
+
+    @Test
+    public void leftJoinTest() {
+        joinTest(JoinType.LEFT, "left");
+    }
+
+    @Test
+    public void rightJoinTest() {
+        joinTest(JoinType.RIGHT, "right");
+    }
+
+    @Test
+    public void innerJoinTest() {
+        joinTest(JoinType.INNER, "inner");
+    }
+
+    @Test
+    public void fullJoinTest() {
+        joinTest(JoinType.FULL, "full");
     }
 }
